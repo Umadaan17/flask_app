1be0127714a4945e50078f550ead5fbb2387f529
Umadaan17 <umadaan17@gmail.com>
2024-07-02 20:13:07 +0530

    push

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask
+from flask import Flask,render_template,request,jsonify
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
+@app.route('/',methods=['GET','POST'])
+def home_page():
+    return render_template('index.html')
 app.run()
